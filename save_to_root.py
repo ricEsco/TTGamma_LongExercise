5a8c13aa03d2b49d18fa132083de67df1598ba32
Jennet Dickinson <dickinson.jennet@gmail.com>
2023-01-10 17:42:18 -0600

    save_to_root

--- save_to_root.py
+++ save_to_root.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python3
 
-from coffea import hist, util
+import hist
+from coffea import util
 from coffea.processor import accumulate
 import numpy as np
 import uproot
 import os
 
-from ttgamma.utils.plotting import RebinHist, SetRangeHist
+from ttgamma.utils.plotting import RebinHist, SetRangeHist, GroupBy
 
 # NOTE: your timestamps will differ!
 outputMC = accumulate(
     [
-        util.load("Outputs/output_MCTTGamma_run20211216_114449.coffea"),
-        util.load("Outputs/output_MCSingleTop_run20211216_124610.coffea"),
-        util.load("Outputs/output_MCTTbar1l_run20211216_121317.coffea"),
-        util.load("Outputs/output_MCTTbar2l_run20211216_122542.coffea"),
-        util.load("Outputs/output_MCWJets_run20211216_142316.coffea"),
-        util.load("Outputs/output_MCZJets_run20211216_130754.coffea"),
-        util.load("Outputs/output_MCOther_run20211216_165125.coffea"),
+        util.load("Outputs/output_MCOther_run20230110_162217.coffea"),      
+        util.load("Outputs/output_MCSingleTop_run20230110_170719.coffea"),  
+        util.load("Outputs/output_MCTTbar1l_run20230110_164315.coffea"),  
+        util.load("Outputs/output_MCTTbar2l_run20230110_170012.coffea"), 
+        util.load("Outputs/output_MCTTGamma_run20230110_171615.coffea"),
+        util.load("Outputs/output_MCWJets_run20230110_161311.coffea"),
+        util.load("Outputs/output_MCZJets_run20230110_155614.coffea"),
     ]
 )
 
-outputData = util.load("Outputs/output_Data_run20211216_171828.coffea")
+outputData = util.load("Outputs/output_Data_run20230110_172734.coffea")
 
-grouping_cat = {
-    "Prompt": slice(1, 2),
-    "MisID": slice(2, 3),
-    "NonPrompt": slice(3, 5),
+groupingCategory = {
+    "NonPrompt": [3j,4j],
+    "MisID": [2j],
+    "Prompt": [1j],
 }
 
-# No QCD and TGJets?
-grouping_dataset = {
-    "ttgamma": [
-        "TTGamma_Dilepton",
-        "TTGamma_SingleLept",
-        "TTGamma_Hadronic",
+groupingMCDatasets = {
+    "ZG": [
+        "ZGamma_01J_5f_lowMass",
     ],
+    "WG": [
+        "WGamma",
+    ],
+
     "other": [
         "TTbarPowheg_Dilepton",
         "TTbarPowheg_Semilept",
         "TTbarPowheg_Hadronic",
-        "W1jets",
         "W2jets",
         "W3jets",
         "W4jets",
-        "DYjetsM10to50",
         "DYjetsM50",
         "ST_s_channel",
         "ST_tW_channel",
@@ -54,45 +54,47 @@ grouping_dataset = {
         "TTWtoLNu",
         "TTWtoQQ",
         "TTZtoLL",
-        "GJets_HT40To100",
-        "GJets_HT100To200",
         "GJets_HT200To400",
         "GJets_HT400To600",
         "GJets_HT600ToInf",
         "ZZ",
         "WZ",
         "WW",
-        #"QCD.",
-        #"TGJets",
-
+        "TGJets"
     ],
-    "WG": [
-        "WGamma",
+    "ttgamma": [
+        "TTGamma_Dilepton",
+        "TTGamma_SingleLept",
+        "TTGamma_Hadronic",
     ],
-    "ZG": [
-        "ZGamma_01J_5f_lowMass",
-    ],
-
 }
+    
+s = hist.tag.Slicer()
 
 if __name__ == "__main__":
+    
+    # Group MC histograms
+    histList = []
+    for samp, sampList in groupingMCDatasets.items():
+        histList += [outputMC[s] for s in sampList]
 
-    h = outputMC["M3"].sum("lepFlavor")
-    h = h.group(
-        "category", hist.Cat(r"category", r"Samples", sorting="placement"), grouping_cat
-    )
-    h = h.group(
-        "dataset", hist.Cat(r"dataset", r"Samples", sorting="placement"), grouping_dataset
-    )
-    h = RebinHist(h, "M3", 10)
-    h = SetRangeHist(h, "M3", 50, 550)
+    outputMCHist = accumulate(histList)
+    for key, histo in outputMCHist.items():
+        if isinstance(histo, hist.Hist):
+            outputMCHist[key] = GroupBy(histo, 'dataset', 'dataset', groupingMCDatasets)
 
-    hData = outputData["M3"].sum("lepFlavor")
-    hData = hData.sum("dataset")
-    hData = hData.sum("category")
-    hData = hData.sum("systematic")
-    hData = RebinHist(hData, "M3", 10)
-    hData = SetRangeHist(hData, "M3", 50, 550)
+    # Group data histograms
+    outputDataHist = accumulate([histo for key, histo in outputData.items()])
+
+    h = outputMCHist['M3']
+    h = h[{'lepFlavor':sum}]
+    h = GroupBy(h, "category", "category", groupingCategory)
+    h = h[{'M3':s[::hist.rebin(10)]}]
+    h = SetRangeHist(h, 'M3', 50, 550)
+
+    hData = outputDataHist['M3'][{'lepFlavor':sum,'category':sum,'systematic':sum,'dataset':sum}]
+    hData = hData[{'M3':s[::hist.rebin(10)]}]
+    hData = SetRangeHist(hData, 'M3', 50, 550)
 
     outdir = "RootFiles"
     if not os.path.exists(outdir):
@@ -100,27 +102,19 @@ if __name__ == "__main__":
 
     outputFile = uproot.recreate(os.path.join(outdir, "M3_Output.root"))
 
-    outputFile["data_obs"] = hData.to_hist()
+    outputFile["data_obs"] = hData
 
-    systematics = h.axis("systematic").identifiers()
+    systematics = h.axes["systematic"]
+
     for _category in ["MisID", "NonPrompt"]:
         for _systematic in systematics:
             histname = f"{_category}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_category}"
-            outputFile[histname] = (
-                h.integrate("category", _category)
-                .integrate("dataset")
-                .integrate("systematic", _systematic)
-                .to_hist()
-            )
+            outputFile[histname] = h[{'dataset':sum,'category':_category,'systematic':_systematic}]
+
     for _dataset in ["ttgamma", "WG", "ZG", "other"]:
         for _systematic in systematics:
             histname = f"{_dataset}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_dataset}"
-            outputFile[histname] = (
-                h.integrate("dataset", _dataset)
-                .integrate("category", "Prompt")
-                .integrate("systematic", _systematic)
-                .to_hist()
-            )
+            outputFile[histname] = h[{'dataset':_dataset,'category':'Prompt','systematic':_systematic}]
 
     outputFile.close()
 
@@ -128,47 +122,30 @@ if __name__ == "__main__":
     nonprompt control region
     '''
 
-    bins = np.array([1.15, 2.5, 4.9, 9, 14.9, 20])  # 1.14 is in the cutbased medium ID.
     # regroup for the different photon categories, summing over all data sets.
-    h = outputMC["photon_chIso"].sum("lepFlavor")
-    h = h.group(
-        "category", hist.Cat(r"category", r"Samples", sorting="placement"), grouping_cat
-    )
-    h = h.group(
-        "dataset", hist.Cat(r"dataset", r"Samples", sorting="placement"), grouping_dataset
-    )
-    h = h.rebin("chIso", hist.Bin("chIso", h.axis("chIso").label, bins))
 
-    hData = outputData["photon_chIso"].sum("lepFlavor")
-    hData = hData.sum("dataset")
-    hData = hData.sum("category")
-    hData = hData.sum("systematic")
-    hData = hData.rebin("chIso", hist.Bin("chIso", hData.axis("chIso").label, bins))
+    h = outputMCHist['photon_chIso']
+    h = h[{"lepFlavor":sum}]
+    h = GroupBy(h, "category", "category", groupingCategory)
 
-    hData.sum("chIso").values()
+    new_bins = np.array([1.15, 2.5, 4.9, 9, 14.9, 20])  # 1.14 is in the cutbased medium ID.   
+    chIso_axis = hist.axis.Variable(new_bins, name='chIso', label=r"Charged Hadron Isolation");
+
+    hData = outputDataHist['photon_chIso'][{'lepFlavor':sum}]
+    hData = hData[{'category':sum,'systematic':sum,'dataset':sum}]
 
     outputFile = uproot.recreate(os.path.join(outdir, "Isolation_Output.root"))
-    outputFile["data_obs"] = hData.to_hist()
+    outputFile["data_obs"] = hData
 
-    systematics = h.axis("systematic").identifiers()
     for _category in ["MisID", "NonPrompt"]:
         for _systematic in systematics:
             histname = f"{_category}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_category}"
-            outputFile[histname] = (
-                h.integrate("category", _category)
-                .integrate("dataset")
-                .integrate("systematic", _systematic)
-                .to_hist()
-            )
+            outputFile[histname] = RebinHist(h[{'dataset':sum,'category':_category,'systematic':_systematic}],chIso=chIso_axis)
+            
     for _dataset in ["ttgamma", "WG", "ZG", "other"]:
         for _systematic in systematics:
             histname = f"{_dataset}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_dataset}"
-            outputFile[histname] = (
-                h.integrate("dataset", _dataset)
-                .integrate("category", "Prompt")
-                .integrate("systematic", _systematic)
-                .to_hist()
-            )
+            outputFile[histname] = RebinHist(h[{'dataset':_dataset,'category':'Prompt','systematic':_systematic}],chIso=chIso_axis)
 
     outputFile.close()
 
@@ -176,52 +153,30 @@ if __name__ == "__main__":
     Mis-ID control region
     '''
 
-    h = outputMC["photon_lepton_mass_3j0t"]
-    h = h.group(
-        "category", hist.Cat(r"category", r"Samples", sorting="placement"), grouping_cat
-    )
-    h = h.group(
-        "dataset", hist.Cat(r"dataset", r"Samples", sorting="placement"), grouping_dataset
-    )
-    h = RebinHist(h, "mass", 20)
-    h = SetRangeHist(h, "mass", 40, 200)
+    h = outputMCHist['photon_lepton_mass_3j0t']
+    h = GroupBy(h, 'category', 'category', groupingCategory)
+    h = h[{'mass':s[::hist.rebin(20)]}]
+    h = SetRangeHist(h,'mass',40,200)
 
-    hData = outputData["photon_lepton_mass_3j0t"]
-    hData = hData.sum("dataset")
-    hData = hData.sum("category")
-    hData = hData.sum("systematic")
-    hData = RebinHist(hData, "mass", 20)
-    hData = SetRangeHist(hData, "mass", 40, 200)
-
-
-    systematics = h.axis("systematic").identifiers()
+    hData = outputDataHist['photon_lepton_mass_3j0t']
+    hData = hData[{'category':sum,'systematic':sum,'dataset':sum}]
+    hData = hData[{'mass':s[::hist.rebin(20)]}]
+    hData = SetRangeHist(hData,'mass',40,200)
 
     for _lepton in ["electron", "muon"]:
         outputFile = uproot.recreate(os.path.join(outdir, f"MisID_Output_{_lepton}.root"))
 
-        outputFile["data_obs"] = hData.integrate("lepFlavor", _lepton).to_hist()
+        outputFile["data_obs"] = hData[{"lepFlavor":_lepton}]
 
-        categories = h.axis("category").identifiers()
-        systematics = h.axis("systematic").identifiers()
+        systematics = h.axes["systematic"]
         for _category in ["MisID", "NonPrompt"]:
             for _systematic in systematics:
                 histname = f"{_category}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_category}"
-                outputFile[histname] = (
-                    h.integrate("category", _category)
-                    .integrate("systematic", _systematic)
-                    .integrate("lepFlavor", _lepton)
-                    .integrate("dataset")
-                    .to_hist()
-                )
+                outputFile[histname] = h[{'category':_category,'systematic':_systematic,'lepFlavor':_lepton,'dataset':sum}]
+
         for _dataset in ["ttgamma", "WG", "ZG", "other"]:
             for _systematic in systematics:
                 histname = f"{_dataset}_{_systematic}" if (not f"{_systematic}" == 'nominal') else f"{_dataset}"
-                outputFile[histname] = (
-                    h.integrate("dataset", _dataset)
-                    .integrate("systematic", _systematic)
-                    .integrate("lepFlavor", _lepton)
-                    .integrate("category", "Prompt")
-                    .to_hist()
-                )
+                outputFile[histname] = h[{'category':'Prompt','systematic':_systematic,'lepFlavor':_lepton,'dataset':_dataset}]
 
         outputFile.close()
