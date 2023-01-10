MC normalization fixed

ttgamma/processor.py
@@ -37,6 +37,12 @@ from .scalefactors import (
 from .utils.crossSections import crossSections, lumis
 from .utils.genParentage import maxHistoryPDGID
 
+def update(events, collections):
+    """Return a shallow copy of events array with some collections swapped out"""
+    out = events
+    for name, value in collections.items():
+        out = ak.with_field(out, value, name)
+    return out
 
 def generatorOverlapRemoval(events, ptCut, etaCut, deltaRCut):
     """Filter generated events with overlapping phase space"""
@@ -233,7 +239,6 @@ class TTGammaProcessor(processor.ProcessorABC):
 
         self.isMC = isMC
         
-#        dataset_axis = hist.axis.StrCategory([], name="dataset", label="Dataset", growth=True)
         lep_axis = hist.axis.StrCategory([], name="lepFlavor", label="Lepton flavor", growth=True)
 
         systematic_axis = hist.axis.StrCategory([], name="systematic", label="Systematic uncertainty", growth=True)
@@ -246,34 +251,32 @@ class TTGammaProcessor(processor.ProcessorABC):
 
         ## Define axis to keep track of photon category
         phoCategory_axis = hist.axis.IntCategory([1, 2, 3, 4, 5], name="category", label=r"Photon Category")
-        #phoCategory_axis.identifiers()[0].label = "Genuine Photon"
-        #phoCategory_axis.identifiers()[1].label = "Misidentified Electron"
-        #phoCategory_axis.identifiers()[2].label = "Hadronic Photon"
-        #phoCategory_axis.identifiers()[3].label = "Hadronic Fake"
         
-        self.make_output = lambda: {
+        ## And the dataset
+        dataset_axis = hist.axis.StrCategory([], name="dataset", label="Dataset", growth=True)
 
+        self.make_output = lambda: { 
             # Test histogram; not needed for final analysis but useful to check things are working
             "all_photon_pt": hist.Hist(
-#                dataset_axis, 
+                dataset_axis,
                 pt_axis),
             ## book histograms for photon pt, eta, and charged hadron isolation
             "photon_pt": hist.Hist(
-#                dataset_axis,
+                dataset_axis,
                 pt_axis,
                 phoCategory_axis,
                 lep_axis,
                 systematic_axis,
             ),
             "photon_eta": hist.Hist(
-#                dataset_axis,
+                dataset_axis,
                 eta_axis,
                 phoCategory_axis,
                 lep_axis,
                 systematic_axis,
             ),  # FIXME 3
             "photon_chIso": hist.Hist(
-#                dataset_axis,
+                dataset_axis,
                 chIso_axis,
                 phoCategory_axis,
                 lep_axis,
@@ -281,7 +284,7 @@ class TTGammaProcessor(processor.ProcessorABC):
             ),
             ## book histogram for photon/lepton mass in a 3j0t region
             "photon_lepton_mass_3j0t": hist.Hist(
-#                dataset_axis,
+                dataset_axis,
                 mass_axis,
                 phoCategory_axis,
                 lep_axis,
@@ -289,7 +292,7 @@ class TTGammaProcessor(processor.ProcessorABC):
             ),
             ## book histogram for M3 variable
             "M3": hist.Hist(
-#                dataset_axis,
+                dataset_axis,
                 m3_axis,
                 phoCategory_axis,
                 lep_axis,
@@ -297,8 +300,9 @@ class TTGammaProcessor(processor.ProcessorABC):
             ),
             "EventCount": processor.value_accumulator(int),
         }
-
+        
     def process(self, events):
+        
         ## Here we pre-compute a few common variables that we will re-use later and add them to the events object
 
         # Temporary patch so we can add photon and lepton four vectors. Not needed for newer versions of NanoAOD
@@ -314,16 +318,17 @@ class TTGammaProcessor(processor.ProcessorABC):
             maxParentFlatten = maxHistoryPDGID(idx, par, num)
             events["GenPart", "maxParent"] = ak.unflatten(maxParentFlatten, num)
 
+        
         shift_systs = [None]
         if self.isMC:
+            # Jet energy systematics 
             shift_systs += ["JESUp", "JESDown", "JERUp", "JERDown"]
-            
+
         return processor.accumulate(self.process_shift(events, name) for name in shift_systs)
 
     def process_shift(self, events, shift_syst=None):
-        output = self.make_output()
-
         dataset = events.metadata["dataset"]
+        output = self.make_output()
 
         # Fill temp hist for testing purposes
         # Feel free to comment this out and copy-paste it to later in the code to check histgrams
@@ -331,7 +336,7 @@ class TTGammaProcessor(processor.ProcessorABC):
         # Remember not to fill it if we are in a shift systematic though
         if shift_syst is None:
             output["all_photon_pt"].fill(
-#                dataset=dataset, 
+                dataset=dataset,
                 pt=ak.flatten(events.Photon.pt)
             )
 
@@ -810,9 +815,6 @@ class TTGammaProcessor(processor.ProcessorABC):
             # find the event weight to be used when filling the histograms
             weightSyst = syst
             
-            if syst == "nominal":
-                output["EventCount"] += len(events)
-
             # in the case of 'nominal', or the jet energy systematics, no weight systematic variation is used (weightSyst=None)
             if syst in ["nominal", "JERUp", "JERDown", "JESUp", "JESDown"]:
                 weightSyst = None
@@ -844,7 +846,7 @@ class TTGammaProcessor(processor.ProcessorABC):
                 # Because of this, we have to use np.asarray(...) since coffea.hist can only accept numpy-compatible arrays
 
                 output["photon_pt"].fill(
-#                    dataset=dataset,
+                    dataset=dataset,
                     pt=np.asarray(leadingPhoton.pt[phosel]),
                     category=np.asarray(phoCategory[phosel]),
                     lepFlavor=lepton,
@@ -853,7 +855,7 @@ class TTGammaProcessor(processor.ProcessorABC):
                 )
 
                 output["photon_eta"].fill(
-#                    dataset=dataset,
+                    dataset=dataset,
                     eta=np.asarray(leadingPhoton.eta[phosel]),
                     category=np.asarray(phoCategory[phosel]),
                     lepFlavor=lepton,
@@ -864,7 +866,7 @@ class TTGammaProcessor(processor.ProcessorABC):
 
                 # fill photon_chIso histogram, using the loosePhotons array (photons passing all cuts, except the charged hadron isolation cuts)
                 output["photon_chIso"].fill(
-#                    dataset=dataset,
+                    dataset=dataset,
                     chIso=np.asarray(leadingPhotonLoose.chIso[phoselLoose]),
                     category=np.asarray(phoCategoryLoose[phoselLoose]),
                     lepFlavor=lepton,
@@ -874,7 +876,7 @@ class TTGammaProcessor(processor.ProcessorABC):
 
                 # fill M3 histogram, for events passing the phosel selection
                 output["M3"].fill(
-#                    dataset=dataset,
+                    dataset=dataset,
                     M3=np.asarray(ak.flatten(M3[phosel])),
                     category=np.asarray(phoCategory[phosel]),
                     lepFlavor=lepton,
@@ -891,7 +893,7 @@ class TTGammaProcessor(processor.ProcessorABC):
 
             for lepton in phosel_3j0t.keys():
                 output["photon_lepton_mass_3j0t"].fill(
-#                    dataset=dataset,
+                    dataset=dataset,
                     mass=np.asarray(gammaMasses[lepton][phosel_3j0t[lepton]]),
                     category=np.asarray(phoCategory[phosel_3j0t[lepton]]),
                     lepFlavor=lepton,
@@ -900,7 +902,9 @@ class TTGammaProcessor(processor.ProcessorABC):
                 )
             # output["photon_lepton_mass_3j0t"].fill()  # FIXME 3
 
-        return {dataset: output}
+            output["EventCount"] = len(events)
+
+        return {dataset:output}
 
     def postprocess(self, accumulator):
         return accumulator

ttgamma/utils/plotting.py
@@ -1,8 +1,209 @@
+import hist
 import matplotlib.pyplot as plt
 from cycler import cycler
-import hist
 import numpy as np
+import itertools
 
+def SetRangeHist(histogram, axisName, lower_bound=None, upper_bound=None):
+    edges = list(histogram.axes[axisName].edges)
+    i_min = edges.index(lower_bound)
+    i_max = edges.index(upper_bound)
+
+    return histogram[{axisName:slice(i_min,i_max)}]
+
+# Taken from Yi-Mu: https://gist.github.com/yimuchen/a5e200c001ef4ea01681a7dd8fe89162#file-integrate_and_rebin-py-L158
+def RebinHist(h, **kwargs):
+    """
+    Rebinning a scikit-hist histogram. 2 types of values can be accepted as the
+    argument values:
+    - Derivatives of the `hist.rebin` argument. In this case we directly use the
+    UHI facilities to perform the rebinning.
+    - A new axis object where all the bin edges lands on the old bin edges of the
+    given histogram. In this case a custom intergration loop is performed to
+    extract the rebinning. Beware that this methods is very slow, as it requires
+    a loop generation of all possible UHI values after the rebinning, so be sure
+    that rebinning is performed as the final step of the histogram reduction. See
+    `_rebin_single_scikit` for more information regarding this method.
+    """
+    h = h.copy()
+    for var, var_val in kwargs.items():
+        if isinstance(var_val, hist.rebin):
+            h = h[{var: var_val}]
+        else:
+            h = _rebin_single_scikit(h, var, var_val)
+    return h
+
+
+def __check_scikit_axis_compat(axis1, axis2):
+    """
+    Checking that axis 2 is rebin-compatible with axis 1. This checks that:
+    1. The two histogram share the same name.
+    2. The edges of the second axis all land on the edges of the first axis.
+    If the two axis are compatible the function will return an array of the bin
+    index of the axis 1 that the bin edges of axis 2 falls on.
+    """
+    assert axis1.name == axis2.name, \
+    'Naming of the axis is required to match'
+    # Getting the new bin edges index for the old bin edges
+    try:
+        return [
+        np.argwhere(axis1.edges == new_edge)[0][0] for new_edge in axis2.edges
+        ]
+    except IndexError as err:
+        raise ValueError(f"Bin edges of the axis {axis2} is incompatible with {axis1}")
+
+
+def _get_all_indices(axis):
+    """
+    Getting all possible (integer) bin index values given a scikit-hep histogram.
+    The special indices of hist.underflow and hist.overflow will be included if the
+    axis in questions has those traits.
+    """
+    idxs = list(range(len(axis)))
+    if axis.traits.underflow:  # Extension to include the under/overflow bins
+        idxs.insert(0, hist.underflow)
+        if axis.traits.overflow:
+            idxs.append(hist.overflow)
+    return idxs
+
+
+def _rebin_single_scikit(h, old_axis, new_axis):
+    """
+    Rebinning a single axis of a scikit-hist histogram. This includes the following
+    routines:
+    - Generating a new scikit hep instance that perserves axis ordering with the
+    exception of the rebinned axis (in place) replacement.
+    - Setting up the integration ranges required to calculate the bin values of the
+    new histogram.
+    - Looping over the UHI values of the new histogram and performing the a
+    summation over the specified range on the old histogram to fill in the new
+    values.
+    As here we have variable number of axis each with variable number of bins, this
+    method will require the use of more old fashioned python looping, which can be
+    very slow for large dimensional histograms with many bins for each axis. So be
+    sure to make rebinning be the final step in histogram reduction.
+    """
+
+    #assert isinstance(h, hist.NamedHist), "Can only process named histograms"
+
+    # Additional type casing
+    if type(old_axis) == str:
+        return _rebin_single_scikit(h, h.axes[old_axis], new_axis)
+    axis_name = old_axis.name
+
+    ## Creating the new histogram instance with identical axis ordering.
+    all_axes = list(h.axes)
+    all_axes[all_axes.index(old_axis)] = new_axis
+    h_rebinned = hist.NamedHist(*all_axes, storage=h._storage_type())
+
+    # Getting the all possible bin indices for all axes in the old histogram
+    bin_idx_dict = {ax.name: _get_all_indices(ax) for ax in h.axes}
+
+    # Getting the new bin edges index for the old bin edges
+    new_bin_edge_idx = __check_scikit_axis_compat(old_axis, new_axis)
+    if new_axis.traits.underflow:  # Adding additional underflow/overflow
+        new_bin_edge_idx.insert(0, bin_idx_dict[axis_name][0])
+        if new_axis.traits.overflow:
+            new_bin_edge_idx.append(bin_idx_dict[axis_name][-1])
+
+    # Generating a the int range pair. Additional parsing will be required for the
+    # under/overflow bins
+
+    def make_slice(index):
+
+        start = new_bin_edge_idx[index]
+        stop = new_bin_edge_idx[index + 1]
+        if start == hist.underflow:
+            start = -1
+        if stop == hist.overflow:
+            stop = len(old_axis)
+        return slice(int(start), int(stop))
+
+    new_axis_idx = _get_all_indices(new_axis)
+    new_int_slice = [make_slice(i) for i in range(len(new_axis_idx))]
+    assert len(new_axis_idx) == len(new_bin_edge_idx) - 1
+
+    new_idx_dict = bin_idx_dict.copy()
+    new_idx_dict[axis_name] = new_axis_idx
+    bin_idx_dict[axis_name] = new_int_slice
+
+    name_list = list(bin_idx_dict.keys())
+    new_idx = [x for x in itertools.product(*[x for x in new_idx_dict.values()])]
+    old_int = [x for x in itertools.product(*[x for x in bin_idx_dict.values()])]
+
+    for o, n in zip(old_int, new_idx):
+        n_uhi = {name: n[name_idx] for name_idx, name in enumerate(name_list)}
+        o_uhi = {name: o[name_idx] for name_idx, name in enumerate(name_list)}
+        # Single variable histogram, with just the axis of interest
+        h_rebinned[n_uhi] = integrate_hist_scikit(h, **o_uhi)
+
+    return h_rebinned
+
+def integrate_hist_scikit(h, **kwargs):
+    """
+    Given a scikit-hist histogram object return a reduced histogram with specified
+    axes integrated out.
+    For scikit-hist histograms, the integration should be formed in 3 steps:
+    - slicing the histogram to contain only the range of interest
+    - Setting overflow values to 0 (excluding the values from future calculations)
+    - Summing over the axes of interest.
+    The latter 2 steps will only be carried out if the var_slice doesn't uniquely
+    identify a singular bin in the histogram axis
+    """
+    # Reduction in parallel.
+    r = h[kwargs]
+    for var, var_slice in kwargs.items():
+        # In the case that histogram has been reduced to singular value simple return
+        #if not isinstance(r, hist.NamedHist):
+        #    return r
+        if var in [x.name for x in r.axes]:
+            ax = h.axes[var]
+            
+            get_underflow = var_slice.start == None or var_slice.start == -1
+            get_overflow = var_slice.stop == None or var_slice.stop == len(ax)
+            if not get_underflow and ax.traits.underflow:
+                r[{var: hist.underflow}] = np.zeros_like(r[{var: hist.underflow}])
+            if not get_overflow and ax.traits.overflow:
+                r[{var: hist.overflow}] = np.zeros_like(r[{var: hist.overflow}])
+                
+            # Sum over all remaining elements on axis
+            r = r[{var: sum}]
+    return r
+
+# Thanks for this, Nick and Andrzej ;)
+def GroupBy(h, oldname, newname, grouping):
+    hnew = hist.Hist(
+        hist.axis.StrCategory(grouping, name=newname),
+        *(ax for ax in h.axes if ax.name != oldname),
+        storage=h._storage_type,
+    )
+    for i, indices in enumerate(grouping.values()):
+        hnew.view(flow=True)[i] = h[{oldname: indices}][{oldname: sum}].view(flow=True)
+
+    return hnew
+
+def GroupBy2(h, oldname, newname, grouping):
+    hnew = hist.Hist(
+        hist.axis.StrCategory(grouping, name=newname),
+        *(ax for ax in h.axes if ax.name != oldname),
+        storage=h._storage_type,
+    )
+    for i, indices in enumerate(grouping.values()):
+        hnew.view(flow=True)[i] = h[{oldname: indices}].view(flow=True)
+
+    return hnew
+
+def SelectAxis(histogram, axisNames, to_select):
+    selection_dict = {}
+    axis_dict = {}
+
+    for i, axis in enumerate(axisNames):
+        bins = histogram.axes[axis]
+        j = bins.index(to_select[i])
+        selection_dict[axis] = j
+        axis_dict[axis] = sum
+
+    return histogram[selection_dict][axis_dict]
 
 def plotWithRatio(
     h,
@@ -10,7 +211,6 @@ def plotWithRatio(
     overlay,
     stacked=True,
     density=False,
-    invertStack=True,
     lumi=35.9,
     label="CMS Preliminary",
     colors=None,
@@ -20,7 +220,7 @@ def plotWithRatio(
     logY=False,
     extraText=None,
     leg="upper right",
-    binwnorm=None,
+    binwnorm=None
 ):
 
     # make a nice ratio plot
@@ -53,62 +253,49 @@ def plotWithRatio(
             colors = colors[_n::-1]
         ax.set_prop_cycle(cycler(color=colors))
 
-    fill_opts = {"edgecolor": (0, 0, 0, 0.3), "alpha": 0.8}
-    error_opts = {
-        "label": "Stat. Unc.",
-        "hatch": "///",
-        "facecolor": "none",
-        "edgecolor": (0, 0, 0, 0.5),
-        "linewidth": 0,
-    }
-    if not stacked:
-        error_opts = None
-        fill_opts = None
-    data_err_opts = {
-        "linestyle": "none",
-        "marker": ".",
-        "markersize": 10.0,
-        "color": "k",
-        "elinewidth": 1,
-        #        'emarker': '_'
-    }
-
-    if invertStack:
-        if type(h._axes[0]) == hist.hist_tools.Cat:
-            h._axes[0]._sorted.reverse()
-    hist.plot1d(
-        h,
+    h.plot(
         overlay=overlay,
         ax=ax,
-        clear=False,
         stack=stacked,
-        density=density,
-        line_opts=None,
-        fill_opts=fill_opts,
-        error_opts=error_opts,
-        binwnorm=binwnorm,
+        histtype='fill',
+        binwnorm=binwnorm
     )
-    # if invertStack:
-    #     if type(h._axes[0])==hist.hist_tools.Cat:
-    #         h._axes[0]._sorted.reverse()
 
     if not hData is None:
-        hist.plot1d(
-            hData,
-            #                    overlay=overlay,
-            ax=ax,
-            clear=False,
-            error_opts=data_err_opts,
-            binwnorm=binwnorm,
-        )
+        
+        if binwnorm:
+            ax.errorbar(x=hData.axes[0].centers,
+                        y=hData.values()/np.diff(hData.axes[0].edges),
+                        yerr=hist.intervals.poisson_interval(hData.values())/np.diff(hData.axes[0].edges),
+                        color='black',
+                        marker='.',
+                        markersize=10,
+                        linewidth=0,
+                        elinewidth=1,
+                        label="Data",
+            )
+        else:
+            ax.errorbar(x=hData.axes[0].centers,
+                        y=hData.values(),
+                        yerr=hist.intervals.poisson_interval(hData.values()),
+                        color='black',
+                        marker='.',
+                        markersize=10,
+                        linewidth=0,
+                        elinewidth=1,
+                        label="Data",
+            )
+        
+    if not binwnorm is None:
+        ax.set_ylabel(f"<Events/{binwnorm}>")
+        if "[" in ax.get_xlabel():
+            units = ax.get_xlabel().split("[")[-1].split("]")[0]
+            ax.set_ylabel(f"<Events / {binwnorm} {units}>")
+    else:
+        ax.set_ylabel('Events')
 
     ax.autoscale(axis="x", tight=True)
     ax.set_ylim(0, None)
-    if not binwnorm is None:
-        ax.set_ylabel(f"<Counts/{binwnorm}>")
-        if "[" in ax.get_xlabel():
-            units = ax.get_xlabel().split("[")[-1].split("]")[0]
-            ax.set_ylabel(f"<Counts / {binwnorm} {units}>")
 
     ax.set_xlabel(None)
 
@@ -123,23 +310,23 @@ def plotWithRatio(
         leg_loc = "upper left"
 
     if not leg is None:
-        handles, labels = ax.get_legend_handles_labels()
-        if not hData is None:
-            handles = handles[-1:] + handles[:-1]
-            labels = ["Data"] + labels[:-1]
-        ax.legend(handles, labels, bbox_to_anchor=leg_anchor, loc=leg_loc)
-
+        ax.legend(bbox_to_anchor=leg_anchor, loc=leg_loc)
+        
     if not hData is None:
-        hist.plotratio(
-            hData,
-            h.sum(overlay),
-            ax=rax,
-            error_opts=data_err_opts,
-            denom_fill_opts={},
-            guide_opts={},
-            unc="num",
+        
+        hist_1_values, hist_2_values = hData.values(), h[{overlay:sum}].values()
+    
+        ratios = hist_1_values / hist_2_values
+        ratio_uncert = hist.intervals.ratio_uncertainty(
+            num=hist_1_values,
+            denom=hist_2_values,
+            uncertainty_type="poisson",
+            
         )
-        rax.set_ylabel("Ratio")
+        # ratio: plot the ratios using Matplotlib errorbar or bar
+        hist.plot.plot_ratio_array(
+            hData, ratios, ratio_uncert, ax=rax, uncert_draw_type='bar',
+        );
 
         rax.set_ylim(ratioRange[0], ratioRange[1])
 
@@ -184,23 +371,4 @@ def plotWithRatio(
         verticalalignment="bottom",
         transform=ax.transAxes,
     )
-
-def SetRangeHist(histogram, axisName, lower_bound=None, upper_bound=None):
-    s = hist.tag.Slicer()
-    return histogram[{axisName:s[lower_bound:upper_bound]}]
-
-def RebinHist(histogram, axisName, rebinN=1):
-    s = hist.tag.Slicer()
-    return histogram[{axisName:s[::hist.rebin(rebinN)]}]
-
-# Thanks for this, Andrzej ;)
-def GroupBy(h, oldname, newname, grouping):
-    hnew = hist.Hist(
-        hist.axis.StrCategory(grouping, name=newname),
-        *(ax for ax in h.axes if ax.name != oldname),
-        storage=h._storage_type,
-    )
-    for i, indices in enumerate(grouping.values()):
-        hnew.view(flow=True)[i] = h[{oldname: indices}][{oldname: sum}].view(flow=True)
-
-    return hnew
+    